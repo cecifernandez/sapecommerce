bdeb4ce77e3a2e9906f44367bb99e870ac2dcb3a
cecifernandez <mceciliafernandezst@gmail.com>
2025-04-14 20:04:59 -0300

    final commit

---
+++ webapp/controller/NotFound.controller.js
@@ -0,0 +1,23 @@
+sap.ui.define(["./BaseController"], function (BaseController) {
+  "use strict";
+
+  return BaseController.extend("ui5.starwarsecommerce.controller.NotFound", {
+    onInit: function () {
+      var oRouter, oTarget;
+
+      oRouter = this.getRouter();
+      oTarget = oRouter.getTarget("notFound");
+      oTarget.attachDisplay(function (oEvent) {
+        this._oData = oEvent.getParameter("data");
+      }, this);
+    },
+    onNavBack: function () {
+      if (this._oData && this._oData.fromTarget) {
+        this.getRouter().getTargets().display(this._oData.fromTarget);
+        delete this._oData.fromTarget;
+        return;
+      }
+      BaseController.prototype.onNavBack.apply(this, arguments);
+    },
+  });
+});
